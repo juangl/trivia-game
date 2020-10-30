1eb5218080b15470b56a02c14b56cc3975530d53
Juan Je Garcia <juanjegarcialopez@gmail.com>
2020-10-29 20:29:08 -0600

    format

--- src/appState/AppStateContext.test.tsx
+++ src/appState/AppStateContext.test.tsx
@@ -4,8 +4,8 @@ import { renderHook } from "@testing-library/react-hooks";
 describe("<AppStateContextProvider />", () => {
     it("should throw if dispatch is called in a component out of the AppStateContextProvider tree", () => {
         let { result } = renderHook(() => useAppStateContext());
-
         let dispatch = result.current[1];
+
         expect(() => dispatch(resetState())).toThrowError(
             /Please wrap your component with the AppStateProvider/i
         );
